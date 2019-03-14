5d592023ff6e3025f498c409daefb539bf55b7c3
Oliver <oliver.grant@gmail.com>
2019-03-14 11:48:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if @status == "completed"
+    if @status == "complete"
       @sender.deposit(@amount)
       @receiver.deposit(-@amount)
       @status = "reversed"
